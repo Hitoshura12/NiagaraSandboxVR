e511662d78c0d4601da04157323703ece9924b68
Hitoshura12 <sgoungaf@gmail.com>
2023-09-05 12:24:49 +0100

    added VR expansion plugin

---
+++ Plugins/VRExpansionPlugin/VRExpansionPlugin/Intermediate/Build/Win64/UnrealGame/Inc/VRExpansionPlugin/UHT/VRAIPerceptionOverrides.generated.h
@@ -0,0 +1,186 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+// IWYU pragma: private, include "Misc/VRAIPerceptionOverrides.h"
+#include "UObject/ObjectMacros.h"
+#include "UObject/ScriptMacros.h"
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+#ifdef VREXPANSIONPLUGIN_VRAIPerceptionOverrides_generated_h
+#error "VRAIPerceptionOverrides.generated.h already included, missing '#pragma once' in VRAIPerceptionOverrides.h"
+#endif
+#define VREXPANSIONPLUGIN_VRAIPerceptionOverrides_generated_h
+
+#define FID_VRExpPluginExp_Plugins_VRExpansionPlugin_VRExpansionPlugin_Source_VRExpansionPlugin_Public_Misc_VRAIPerceptionOverrides_h_24_SPARSE_DATA
+#define FID_VRExpPluginExp_Plugins_VRExpansionPlugin_VRExpansionPlugin_Source_VRExpansionPlugin_Public_Misc_VRAIPerceptionOverrides_h_24_RPC_WRAPPERS
+#define FID_VRExpPluginExp_Plugins_VRExpansionPlugin_VRExpansionPlugin_Source_VRExpansionPlugin_Public_Misc_VRAIPerceptionOverrides_h_24_RPC_WRAPPERS_NO_PURE_DECLS
+#define FID_VRExpPluginExp_Plugins_VRExpansionPlugin_VRExpansionPlugin_Source_VRExpansionPlugin_Public_Misc_VRAIPerceptionOverrides_h_24_ACCESSORS
+#define FID_VRExpPluginExp_Plugins_VRExpansionPlugin_VRExpansionPlugin_Source_VRExpansionPlugin_Public_Misc_VRAIPerceptionOverrides_h_24_INCLASS_NO_PURE_DECLS \
+private: \
+	static void StaticRegisterNativesUAISenseConfig_Sight_VR(); \
+	friend struct Z_Construct_UClass_UAISenseConfig_Sight_VR_Statics; \
+public: \
+	DECLARE_CLASS(UAISenseConfig_Sight_VR, UAISenseConfig, COMPILED_IN_FLAGS(0 | CLASS_Config), CASTCLASS_None, TEXT("/Script/VRExpansionPlugin"), NO_API) \
+	DECLARE_SERIALIZER(UAISenseConfig_Sight_VR)
+
+
+#define FID_VRExpPluginExp_Plugins_VRExpansionPlugin_VRExpansionPlugin_Source_VRExpansionPlugin_Public_Misc_VRAIPerceptionOverrides_h_24_INCLASS \
+private: \
+	static void StaticRegisterNativesUAISenseConfig_Sight_VR(); \
+	friend struct Z_Construct_UClass_UAISenseConfig_Sight_VR_Statics; \
+public: \
+	DECLARE_CLASS(UAISenseConfig_Sight_VR, UAISenseConfig, COMPILED_IN_FLAGS(0 | CLASS_Config), CASTCLASS_None, TEXT("/Script/VRExpansionPlugin"), NO_API) \
+	DECLARE_SERIALIZER(UAISenseConfig_Sight_VR)
+
+
+#define FID_VRExpPluginExp_Plugins_VRExpansionPlugin_VRExpansionPlugin_Source_VRExpansionPlugin_Public_Misc_VRAIPerceptionOverrides_h_24_STANDARD_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API UAISenseConfig_Sight_VR(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get()); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(UAISenseConfig_Sight_VR) \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, UAISenseConfig_Sight_VR); \
+	DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(UAISenseConfig_Sight_VR); \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API UAISenseConfig_Sight_VR(UAISenseConfig_Sight_VR&&); \
+	NO_API UAISenseConfig_Sight_VR(const UAISenseConfig_Sight_VR&); \
+public: \
+	NO_API virtual ~UAISenseConfig_Sight_VR();
+
+
+#define FID_VRExpPluginExp_Plugins_VRExpansionPlugin_VRExpansionPlugin_Source_VRExpansionPlugin_Public_Misc_VRAIPerceptionOverrides_h_24_ENHANCED_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API UAISenseConfig_Sight_VR(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get()) : Super(ObjectInitializer) { }; \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API UAISenseConfig_Sight_VR(UAISenseConfig_Sight_VR&&); \
+	NO_API UAISenseConfig_Sight_VR(const UAISenseConfig_Sight_VR&); \
+public: \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, UAISenseConfig_Sight_VR); \
+	DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(UAISenseConfig_Sight_VR); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(UAISenseConfig_Sight_VR) \
+	NO_API virtual ~UAISenseConfig_Sight_VR();
+
+
+#define FID_VRExpPluginExp_Plugins_VRExpansionPlugin_VRExpansionPlugin_Source_VRExpansionPlugin_Public_Misc_VRAIPerceptionOverrides_h_21_PROLOG
+#define FID_VRExpPluginExp_Plugins_VRExpansionPlugin_VRExpansionPlugin_Source_VRExpansionPlugin_Public_Misc_VRAIPerceptionOverrides_h_24_GENERATED_BODY_LEGACY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	FID_VRExpPluginExp_Plugins_VRExpansionPlugin_VRExpansionPlugin_Source_VRExpansionPlugin_Public_Misc_VRAIPerceptionOverrides_h_24_SPARSE_DATA \
+	FID_VRExpPluginExp_Plugins_VRExpansionPlugin_VRExpansionPlugin_Source_VRExpansionPlugin_Public_Misc_VRAIPerceptionOverrides_h_24_RPC_WRAPPERS \
+	FID_VRExpPluginExp_Plugins_VRExpansionPlugin_VRExpansionPlugin_Source_VRExpansionPlugin_Public_Misc_VRAIPerceptionOverrides_h_24_ACCESSORS \
+	FID_VRExpPluginExp_Plugins_VRExpansionPlugin_VRExpansionPlugin_Source_VRExpansionPlugin_Public_Misc_VRAIPerceptionOverrides_h_24_INCLASS \
+	FID_VRExpPluginExp_Plugins_VRExpansionPlugin_VRExpansionPlugin_Source_VRExpansionPlugin_Public_Misc_VRAIPerceptionOverrides_h_24_STANDARD_CONSTRUCTORS \
+public: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#define FID_VRExpPluginExp_Plugins_VRExpansionPlugin_VRExpansionPlugin_Source_VRExpansionPlugin_Public_Misc_VRAIPerceptionOverrides_h_24_GENERATED_BODY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	FID_VRExpPluginExp_Plugins_VRExpansionPlugin_VRExpansionPlugin_Source_VRExpansionPlugin_Public_Misc_VRAIPerceptionOverrides_h_24_SPARSE_DATA \
+	FID_VRExpPluginExp_Plugins_VRExpansionPlugin_VRExpansionPlugin_Source_VRExpansionPlugin_Public_Misc_VRAIPerceptionOverrides_h_24_RPC_WRAPPERS_NO_PURE_DECLS \
+	FID_VRExpPluginExp_Plugins_VRExpansionPlugin_VRExpansionPlugin_Source_VRExpansionPlugin_Public_Misc_VRAIPerceptionOverrides_h_24_ACCESSORS \
+	FID_VRExpPluginExp_Plugins_VRExpansionPlugin_VRExpansionPlugin_Source_VRExpansionPlugin_Public_Misc_VRAIPerceptionOverrides_h_24_INCLASS_NO_PURE_DECLS \
+	FID_VRExpPluginExp_Plugins_VRExpansionPlugin_VRExpansionPlugin_Source_VRExpansionPlugin_Public_Misc_VRAIPerceptionOverrides_h_24_ENHANCED_CONSTRUCTORS \
+static_assert(false, "Unknown access specifier for GENERATED_BODY() macro in class AISenseConfig_Sight_VR."); \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+template<> VREXPANSIONPLUGIN_API UClass* StaticClass<class UAISenseConfig_Sight_VR>();
+
+#define FID_VRExpPluginExp_Plugins_VRExpansionPlugin_VRExpansionPlugin_Source_VRExpansionPlugin_Public_Misc_VRAIPerceptionOverrides_h_86_GENERATED_BODY \
+	friend struct Z_Construct_UScriptStruct_FAISightEventVR_Statics; \
+	static class UScriptStruct* StaticStruct();
+
+
+template<> VREXPANSIONPLUGIN_API UScriptStruct* StaticStruct<struct FAISightEventVR>();
+
+#define FID_VRExpPluginExp_Plugins_VRExpansionPlugin_VRExpansionPlugin_Source_VRExpansionPlugin_Public_Misc_VRAIPerceptionOverrides_h_187_SPARSE_DATA
+#define FID_VRExpPluginExp_Plugins_VRExpansionPlugin_VRExpansionPlugin_Source_VRExpansionPlugin_Public_Misc_VRAIPerceptionOverrides_h_187_RPC_WRAPPERS
+#define FID_VRExpPluginExp_Plugins_VRExpansionPlugin_VRExpansionPlugin_Source_VRExpansionPlugin_Public_Misc_VRAIPerceptionOverrides_h_187_RPC_WRAPPERS_NO_PURE_DECLS
+#define FID_VRExpPluginExp_Plugins_VRExpansionPlugin_VRExpansionPlugin_Source_VRExpansionPlugin_Public_Misc_VRAIPerceptionOverrides_h_187_ACCESSORS
+#define FID_VRExpPluginExp_Plugins_VRExpansionPlugin_VRExpansionPlugin_Source_VRExpansionPlugin_Public_Misc_VRAIPerceptionOverrides_h_187_INCLASS_NO_PURE_DECLS \
+private: \
+	static void StaticRegisterNativesUAISense_Sight_VR(); \
+	friend struct Z_Construct_UClass_UAISense_Sight_VR_Statics; \
+public: \
+	DECLARE_CLASS(UAISense_Sight_VR, UAISense, COMPILED_IN_FLAGS(0 | CLASS_Config), CASTCLASS_None, TEXT("/Script/VRExpansionPlugin"), NO_API) \
+	DECLARE_SERIALIZER(UAISense_Sight_VR) \
+	static const TCHAR* StaticConfigName() {return TEXT("Game");} \
+
+
+
+#define FID_VRExpPluginExp_Plugins_VRExpansionPlugin_VRExpansionPlugin_Source_VRExpansionPlugin_Public_Misc_VRAIPerceptionOverrides_h_187_INCLASS \
+private: \
+	static void StaticRegisterNativesUAISense_Sight_VR(); \
+	friend struct Z_Construct_UClass_UAISense_Sight_VR_Statics; \
+public: \
+	DECLARE_CLASS(UAISense_Sight_VR, UAISense, COMPILED_IN_FLAGS(0 | CLASS_Config), CASTCLASS_None, TEXT("/Script/VRExpansionPlugin"), NO_API) \
+	DECLARE_SERIALIZER(UAISense_Sight_VR) \
+	static const TCHAR* StaticConfigName() {return TEXT("Game");} \
+
+
+
+#define FID_VRExpPluginExp_Plugins_VRExpansionPlugin_VRExpansionPlugin_Source_VRExpansionPlugin_Public_Misc_VRAIPerceptionOverrides_h_187_STANDARD_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API UAISense_Sight_VR(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get()); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(UAISense_Sight_VR) \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, UAISense_Sight_VR); \
+	DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(UAISense_Sight_VR); \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API UAISense_Sight_VR(UAISense_Sight_VR&&); \
+	NO_API UAISense_Sight_VR(const UAISense_Sight_VR&); \
+public: \
+	NO_API virtual ~UAISense_Sight_VR();
+
+
+#define FID_VRExpPluginExp_Plugins_VRExpansionPlugin_VRExpansionPlugin_Source_VRExpansionPlugin_Public_Misc_VRAIPerceptionOverrides_h_187_ENHANCED_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API UAISense_Sight_VR(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get()) : Super(ObjectInitializer) { }; \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API UAISense_Sight_VR(UAISense_Sight_VR&&); \
+	NO_API UAISense_Sight_VR(const UAISense_Sight_VR&); \
+public: \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, UAISense_Sight_VR); \
+	DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(UAISense_Sight_VR); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(UAISense_Sight_VR) \
+	NO_API virtual ~UAISense_Sight_VR();
+
+
+#define FID_VRExpPluginExp_Plugins_VRExpansionPlugin_VRExpansionPlugin_Source_VRExpansionPlugin_Public_Misc_VRAIPerceptionOverrides_h_184_PROLOG
+#define FID_VRExpPluginExp_Plugins_VRExpansionPlugin_VRExpansionPlugin_Source_VRExpansionPlugin_Public_Misc_VRAIPerceptionOverrides_h_187_GENERATED_BODY_LEGACY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	FID_VRExpPluginExp_Plugins_VRExpansionPlugin_VRExpansionPlugin_Source_VRExpansionPlugin_Public_Misc_VRAIPerceptionOverrides_h_187_SPARSE_DATA \
+	FID_VRExpPluginExp_Plugins_VRExpansionPlugin_VRExpansionPlugin_Source_VRExpansionPlugin_Public_Misc_VRAIPerceptionOverrides_h_187_RPC_WRAPPERS \
+	FID_VRExpPluginExp_Plugins_VRExpansionPlugin_VRExpansionPlugin_Source_VRExpansionPlugin_Public_Misc_VRAIPerceptionOverrides_h_187_ACCESSORS \
+	FID_VRExpPluginExp_Plugins_VRExpansionPlugin_VRExpansionPlugin_Source_VRExpansionPlugin_Public_Misc_VRAIPerceptionOverrides_h_187_INCLASS \
+	FID_VRExpPluginExp_Plugins_VRExpansionPlugin_VRExpansionPlugin_Source_VRExpansionPlugin_Public_Misc_VRAIPerceptionOverrides_h_187_STANDARD_CONSTRUCTORS \
+public: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#define FID_VRExpPluginExp_Plugins_VRExpansionPlugin_VRExpansionPlugin_Source_VRExpansionPlugin_Public_Misc_VRAIPerceptionOverrides_h_187_GENERATED_BODY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	FID_VRExpPluginExp_Plugins_VRExpansionPlugin_VRExpansionPlugin_Source_VRExpansionPlugin_Public_Misc_VRAIPerceptionOverrides_h_187_SPARSE_DATA \
+	FID_VRExpPluginExp_Plugins_VRExpansionPlugin_VRExpansionPlugin_Source_VRExpansionPlugin_Public_Misc_VRAIPerceptionOverrides_h_187_RPC_WRAPPERS_NO_PURE_DECLS \
+	FID_VRExpPluginExp_Plugins_VRExpansionPlugin_VRExpansionPlugin_Source_VRExpansionPlugin_Public_Misc_VRAIPerceptionOverrides_h_187_ACCESSORS \
+	FID_VRExpPluginExp_Plugins_VRExpansionPlugin_VRExpansionPlugin_Source_VRExpansionPlugin_Public_Misc_VRAIPerceptionOverrides_h_187_INCLASS_NO_PURE_DECLS \
+	FID_VRExpPluginExp_Plugins_VRExpansionPlugin_VRExpansionPlugin_Source_VRExpansionPlugin_Public_Misc_VRAIPerceptionOverrides_h_187_ENHANCED_CONSTRUCTORS \
+static_assert(false, "Unknown access specifier for GENERATED_BODY() macro in class AISense_Sight_VR."); \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+template<> VREXPANSIONPLUGIN_API UClass* StaticClass<class UAISense_Sight_VR>();
+
+#undef CURRENT_FILE_ID
+#define CURRENT_FILE_ID FID_VRExpPluginExp_Plugins_VRExpansionPlugin_VRExpansionPlugin_Source_VRExpansionPlugin_Public_Misc_VRAIPerceptionOverrides_h
+
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
